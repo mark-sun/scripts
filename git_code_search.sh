d5deede579232138a5134636a7f2854841f4f8b9
Mark Sun <mark.sun@robinhood.com>
2019-07-31 16:51:42 -0700

    add instructions

--- git_code_search.sh
+++ git_code_search.sh
@@ -2,3 +2,12 @@
 
 # git grep code search in non-test python files.
 git grep --break --heading --line-number "$1" -- ":(exclude)*test*.py" "*.py"
+
+# Then, run:
+# chmod +x <path to this file>
+
+# Then, add a line in the ~/.bash_profile:
+# alias csp='~/ws/git_code_search.sh'
+
+# Finally, run:
+# source ~/.bash_profile
