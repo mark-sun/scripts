a769ccdfcee210ce480210a803d1d3e661642b53
Mark Sun <mark.sun@robinhood.com>
2019-08-08 11:54:45 -0700

    change alias

--- git_code_search.sh
+++ git_code_search.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -x
 
 # git grep code search in non-test python files.
-git grep --break --heading --line-number "$1" -- ":(exclude)*test*.py" "*.py"
+git grep --break --heading --line-number $1 -- ":(exclude)*test*.py" "*.py"
 
 # Then, run:
 # chmod +x <path to this file>
 
 # Then, add a line in the ~/.bash_profile:
-# alias csp='~/ws/git_code_search.sh'
+# alias cs='~/ws/scripts/git_code_search.sh'
 
 # Finally, run:
 # source ~/.bash_profile
